a0e2160bfee889530d8e32e0404bb12a2fd10cd7
MAES Lionel (s) <lionel.maes@dt-c205-07.local>
2018-05-04 16:42:53 +0200

    Initial Commit

---
+++ Lionel_Maes_multec_Werkstuk2_v2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lionel_Maes_multec_Werkstuk2_v2
+//
+//  Created by MAES Lionel (s) on 04/05/2018.
+//  Copyright © 2018 MAES Lionel (s). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
